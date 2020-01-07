2dc4008d1a0217ecd4173086e874225191300044
lososkin <yalososka@gmail.com>
2020-01-08 02:09:00 +0300

    main.py
    fix new maze command

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def create_first_lab(message):
 	markup = telebot.types.ReplyKeyboardMarkup(True)
-	markup.row('/new','up',' ')
+	markup.row('new','up',' ')
 	markup.row('left', 'down', 'right')
 	bot.send_message(message.chat.id, "Приффки", reply_markup=markup)
 	lab=sql.get_lab(message.chat.id)
@@ -49,22 +49,22 @@ def repeat_all_messages(message):
 		lab=lab.to_text()
 		sql.update_lab(message.chat.id,lab)
 		bot.send_message(message.chat.id, lab)
-		if is_win==1:
-			message_text="Это вин! Вот тебе новый:"
+		if is_win==1 or message.text=='new':
+			message_text="Вот тебе новый:"
 			bot.send_message(message.chat.id, message_text)
 			lab=labirint.Labirint(height,width,wall_cell,space_cell,gamer_cell,1)
 			lab=lab.to_text()
 			sql.update_lab(message.chat.id,lab)
 			bot.send_message(message.chat.id, lab)
 
-@bot.message_handler(commands=['new'])
-def create_new_lab(message):
-	message_text="Вот тебе новый:"
-	bot.send_message(message.chat.id, message_text)
-	lab=labirint.Labirint(height,width,wall_cell,space_cell,gamer_cell,1)
-	lab=lab.to_text()
-	sql.update_lab(message.chat.id,lab)
-	bot.send_message(message.chat.id, lab)
+#@bot.message_handler(commands=['new'])
+#def create_new_lab(message):
+#	message_text="Вот тебе новый:"
+#	bot.send_message(message.chat.id, message_text)
+#	lab=labirint.Labirint(height,width,wall_cell,space_cell,gamer_cell,1)
+#	lab=lab.to_text()
+#	sql.update_lab(message.chat.id,lab)
+#	bot.send_message(message.chat.id, lab)
 
 if __name__ == '__main__':
 	bot.polling(none_stop=True)
